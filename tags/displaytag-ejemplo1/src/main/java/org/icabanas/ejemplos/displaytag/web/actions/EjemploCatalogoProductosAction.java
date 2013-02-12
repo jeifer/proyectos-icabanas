f9088ca8d32bc3ef91ddd1ca30e51d5dd0bb97b7
pajarokillo <pajarokillo@a2af3cd2-4f60-bf44-bd27-6a38996e419d>
2013-02-12 19:49:13 +0000

    Primer ejemplo con displaytag

---
+++ tags/displaytag-ejemplo1/src/main/java/org/icabanas/ejemplos/displaytag/web/actions/EjemploCatalogoProductosAction.java
@@ -0,0 +1,34 @@
+package org.icabanas.ejemplos.displaytag.web.actions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.struts2.ServletActionContext;
+import org.icabanas.ejemplos.displaytag.modelo.Producto;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class EjemploCatalogoProductosAction {
+
+	public String ejemplo1(){
+		List<Producto> productos = getProductos();
+		
+		ServletActionContext.getRequest().setAttribute("productos", productos);
+		
+		return ActionSupport.SUCCESS;
+	}
+
+	private List<Producto> getProductos() {
+		List<Producto> resultado = new ArrayList<Producto>();
+		
+		resultado.add(new Producto("1","producto1","descripcion1"));
+		resultado.add(new Producto("2","producto2","descripcion2"));
+		resultado.add(new Producto("3","producto3","descripcion3"));
+		resultado.add(new Producto("4","producto4","descripcion4"));
+		resultado.add(new Producto("5","producto5","descripcion5"));
+		resultado.add(new Producto("6","producto6","descripcion6"));
+		resultado.add(new Producto("7","producto7","descripcion7"));
+		
+		return resultado;
+	}
+}
